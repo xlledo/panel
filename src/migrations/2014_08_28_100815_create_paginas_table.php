3a8421d0792c95b7409e8d5ce53810c71465efa0
Miguel <migueltorressanchez@gmail.com>
2014-08-28 13:34:46 +0200

    Inicio paginas

---
+++ src/migrations/2014_08_28_100815_create_paginas_table.php
@@ -0,0 +1,70 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePaginasTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('paginas', function(Blueprint $table)
+		{
+                        $table->increments('id');
+                        $table->softDeletes();
+                        
+                        $table->integer('creado_por')->unsigned();
+                        $table->integer('actualizado_por')->unsigned();
+                        
+                        $table->foreign('creado_por')->references('id')->on('users');
+                        $table->foreign('actualizado_por')->references('id')->on('users');
+                        
+			$table->timestamps();
+		});
+                
+                Schema::create('paginas_i18n',function(Blueprint $table){
+
+                        $table->increments('id');
+                        
+                        $table->integer('item_id')->unsigned();
+                        $table->string('idioma',4);
+                        
+                        $table->string('titulo',255);
+                        $table->text('texto');
+                        
+                });
+                
+                Schema::create('paginas_ficheros', function(Blueprint $table){
+
+                        $table->increments('id');
+                        
+                        $table->integer('pagina_id');
+                        $table->integer('fichero_id');
+                        
+                        $table->string('idioma',4);
+                        
+                        $table->string('titulo', 255);
+                        $table->string('alt', 255);
+                        $table->text('descripcion');
+                        $table->string('enlace',255);
+                        
+                });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('paginas');
+                Schema::drop('paginas_i18n');
+                Schema::drop('paginas_ficheros');
+	}
+
+}
