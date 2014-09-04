25394e1d762d855ad51206e98805a3929a51001e
Ximo Lledó <ximology@gmail.com>
2014-09-04 15:53:01 +0200

    Corregir error en la creación de subcategorías

--- src/Ttt/Panel/Repo/Categoria/EloquentCategoria.php
+++ src/Ttt/Panel/Repo/Categoria/EloquentCategoria.php
@@ -156,7 +156,7 @@ class EloquentCategoria implements CategoriaInterface{
      */
     public function createModel()
     {
-        $class = '\\Ttt\\Panel\\Repo\\Categoriatraducible\\Categoria';
+        $class = '\\Ttt\\Panel\\Repo\\Categoria\\Categoria';
 
         return new $class;
     }
