7471f922ed59dabf91f29c54decb830014dbafef
Miguel <migueltorressanchez@gmail.com>
2014-12-23 11:35:50 +0100

    Los ficheros ahora borran sus miniaturas, el index de ficheros ahora carga con los thumbnails dinamicos

--- src/views/ficheros/index.blade.php
+++ src/views/ficheros/index.blade.php
@@ -68,7 +68,7 @@
 									<tr class="@if($index % 2 == 0) par @else impar @endif">
                                                                                 <td class="td_click">
                                                                                     @if($item->esImagen())
-                                                                                        <img src="{{ $item->getStreamBase64()}}" style="max-width: 50px;"/>
+                                                                                        <img src="{{ $item->getStreamBase64($item->getSize(50))}}" style="max-width: 50px;"/>
                                                                                     @else
                                                                                         <i class="icon-file-text"></i>
                                                                                     @endif
